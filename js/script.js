44864f02ef80d044df32219f8f80182725ac1d5b
Murat Yaşar <murat.yasar@comkom.de>
2024-07-09 09:58:24 +0200

    Add and style navbar

--- js/script.js
+++ js/script.js
@@ -1,1 +1,5 @@
 console.log('JS file initiated!');
+
+document.getElementById('nav-toggle').addEventListener('click', function() {
+  document.querySelector('.nav-menu').classList.toggle('active');
+});
