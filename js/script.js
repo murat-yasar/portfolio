7f6252fa26fd7a0c6485398756fad197544f82dc
Murat Yaşar <murat.yasar@comkom.de>
2024-07-10 14:53:53 +0200

    Clean project

--- js/script.js
+++
@@ -1,5 +0,0 @@
-console.log('JS file initiated!');
-
-document.getElementById('nav-toggle').addEventListener('click', function() {
-  document.querySelector('.nav-menu').classList.toggle('active');
-});
